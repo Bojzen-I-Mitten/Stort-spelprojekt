4d5a5ec6000d07a5f4d5ab394da80f2ec8abd581
Unknown <deankulenovic@gmail.com>
2018-09-10 14:57:45 +0200

    Added a simple manager

---
+++ thomas/ThomasNet/NetworkManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThomasEngine.Network
+{
+    public class NetworkManager : ScriptComponent
+    {
+
+        Server server = new Server();
+        Client client = new Client();
+
+        public override void Start()
+        {
+            server.Start();
+            client.Start();
+            server.SendData("You are Connected To the server");
+
+        }
+        public override void Update()
+        {
+            server.Update();
+            client.Update();
+        }
+        public override void Destroy()
+        {
+            server.Stop();
+            client.Stop();
+            base.Destroy();
+        }
+    }
+}
