0b4b100cfdc92a4103bff3bb2d3a37111b33a982
Jakob Ahrneteg <jakob.ahrneteg@gmail.com>
2018-09-11 11:00:53 +0200

    Removed throw ball script from thomas and added it to concussion ball

--- Concussion Ball/Assets/MoveBall.cs
+++ Concussion Ball/Assets/MoveBall.cs
@@ -12,7 +12,7 @@ public class MoveBall : ScriptComponent
     public override void Update()
     {
         gameObject.transform.position = new Vector3(0, 5, 0);
-      
+        
         //t += Time.DeltaTime;
         //gameObject.transform.position = new Vector3(0, (float)posCurve.GetYFromX((t * 6.5) % 5), 0);
         //gameObject.transform.scale = new Vector3((float)scaleCurve.GetYFromX((t * 6.5) % 5), 1, (float)scaleCurve.GetYFromX((t * 6.5) % 5));
