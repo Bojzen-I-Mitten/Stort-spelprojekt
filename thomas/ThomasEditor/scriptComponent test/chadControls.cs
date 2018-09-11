18ef6288c3ba0f304e0839c3aa767da11fd2d6c3
Nick Wedzinga <oxay@live.com>
2018-09-11 11:57:03 +0200

    Camera follows player object and player correctly manipulates movement through camera direction.

--- thomas/ThomasEditor/scriptComponent test/chadControls.cs
+++ thomas/ThomasEditor/scriptComponent test/chadControls.cs
@@ -12,30 +12,37 @@ namespace ThomasEditor
         //public GameObject aDude;
         public int speed { get; set; }
 
+        Camera chadCamera;
         float t = 0;
 
         public override void Start()
         {
+            chadCamera = this.gameObject.GetComponent<Camera>();
         }
 
         public override void Update()
         {
             t = Time.DeltaTime;
-            float x = gameObject.transform.position.x;
-            float y = gameObject.transform.position.y;
-            float z = gameObject.transform.position.z;
 
-            Vector3 test = gameObject.transform.forward;
-            float test2 = gameObject.transform.forward.z + t * speed;
             
+
             if (Input.GetKey(Input.Keys.W))
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x + gameObject.transform.forward.x * t * speed, gameObject.transform.position.y + gameObject.transform.forward.y * t*speed, gameObject.transform.position.z+ gameObject.transform.forward.z * t*speed);
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x + gameObject.transform.forward.x * t * speed, gameObject.transform.position.y + gameObject.transform.forward.y * t * speed, gameObject.transform.position.z + gameObject.transform.forward.z * t * speed);
+            }
             if (Input.GetKey(Input.Keys.S))
                 gameObject.transform.position = new Vector3(gameObject.transform.position.x - gameObject.transform.forward.x * t * speed, gameObject.transform.position.y - gameObject.transform.forward.y * t * speed, gameObject.transform.position.z - gameObject.transform.forward.z * t * speed);
             if (Input.GetKey(Input.Keys.A))
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x - gameObject.transform.right.x * t * speed, gameObject.transform.position.y - gameObject.transform.right.y * t * speed, gameObject.transform.position.z - gameObject.transform.right.z * t * speed);
+            {
+                chadCamera.gameObject.transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), 0.01f);
+                //gameObject.transform.position = new Vector3(gameObject.transform.position.x - gameObject.transform.right.x * t * speed, gameObject.transform.position.y - gameObject.transform.right.y * t * speed, gameObject.transform.position.z - gameObject.transform.right.z * t * speed);
+
+            }
             if (Input.GetKey(Input.Keys.D))
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x + gameObject.transform.right.x * t * speed, gameObject.transform.position.y + gameObject.transform.right.y * t * speed, gameObject.transform.position.z + gameObject.transform.right.z * t * speed);
+            {
+                chadCamera.gameObject.transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), -0.01f);
+                //gameObject.transform.position = new Vector3(gameObject.transform.position.x + gameObject.transform.right.x * t * speed, gameObject.transform.position.y + gameObject.transform.right.y * t * speed, gameObject.transform.position.z + gameObject.transform.right.z * t * speed);
+            }
         }
     }
 }
