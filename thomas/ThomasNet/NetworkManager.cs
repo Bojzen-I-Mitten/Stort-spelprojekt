50410366ca5351584fc7da765aaa52d195485a97
DESKTOP-KD8IOUS\BTH <oscar@niceguyandersson.com>
2018-09-12 11:05:04 +0200

    Added methods for subscribing data type and callback and sending

--- thomas/ThomasNet/NetworkManager.cs
+++ thomas/ThomasNet/NetworkManager.cs
@@ -9,36 +9,45 @@ using LiteNetLib.Utils;
 
 namespace ThomasEngine.Network
 {
+    public class ExamplePacket
+    {
+        public string test1 { get; set; } = "asd";
+        public int test2 { get; set; } = 123;
+        public Vector3 test3 = new Vector3(0.0F, 0.0F, 0.0F);
+
+        static public void PrintPacket(ExamplePacket packet, NetPeer peer)
+        {
+            ThomasEngine.Debug.Log(System.String.Format("Received: \n {0}\n {1}\n {2}\t{3}\t{4}", packet.test1, packet.test2, packet.test3.x, packet.test3.y, packet.test3.z));
+        }
+    }
+
     public class NetworkManager : ScriptComponent
     {
         Dictionary<int, NetworkID> networkIDObjects = new Dictionary<int, NetworkID>();
         int iD = -1;
 
-      
+        private readonly NetPacketProcessor netPacketProcessor = new NetPacketProcessor();
+
         private EventBasedNetListener listener;
         private NetManager netManager;
         private NetDataWriter writer;
-        public string IP { get; set; } = "localhost"; 
+        public string IP { get; set; } = "localhost";
         public bool Server { get; set; } = false;
-        //       public int port { get; set; } = 9050;
+        //public int port { get; set; } = 9050;
         public static NetworkManager instance;
+        public ExamplePacket testPacket = new ExamplePacket();
         public override void Start()
         {
             listener = new EventBasedNetListener();
             netManager = new NetManager(listener);
             instance = this;
             writer = new NetDataWriter();
-            if (!Server)
+            if (!Server) //client
             {
                 netManager.Start();
                 netManager.Connect(IP /* host ip or name */, 9050 /* port */, "SomeConnectionKey" /* text key or NetDataWriter */);
-
-                //  ExecuteEvent(networkIDObjects);
-                //  SendDataOverEvent("client send data", DeliveryMethod.ReliableOrdered);
-                //  client.SendDataOverEvent(2, DeliveryMethod.ReliableOrdered);
-              //    SendDataOverEvent(0.0f, 5f, 0f, DeliveryMethod.ReliableOrdered);
             }
-            else
+            else //server
             {
                 netManager.Start(9050 /* port */);
 
@@ -51,11 +60,11 @@ namespace ThomasEngine.Network
                         request.Reject();
                 };
 
-                 SendDataOverEvent("You are Connected To the server", DeliveryMethod.ReliableOrdered);
-
-              //  SendDataOverEvent(0.0f, 2.2f, 0.0f, DeliveryMethod.ReliableOrdered);
-              
+                 SendDataOverEvent(testPacket, DeliveryMethod.ReliableOrdered);
             }
+
+            SubscribeDataType<ExamplePacket>(ExamplePacket.PrintPacket);
+
             ExecuteEvent();
 
         }
@@ -78,38 +87,20 @@ namespace ThomasEngine.Network
 
         public void ExecuteEvent()
         {
-            listener.NetworkReceiveEvent += (fromPeer, dataReader, deliveryMethod) =>
-            {
-                 ThomasEngine.Debug.Log(dataReader.GetString());
-                // Console.WriteLine("We got: {0}" , dataReader.GetString(100 /* max length of string */));  dataReader.GetString(100 /* max length of string */)+
-               // networkIDObjects[]
-
-                dataReader.Recycle();
-            };
+            listener.NetworkReceiveEvent += (fromPeer, dataReader, deliveryMethod) => netPacketProcessor.ReadAllPackets(dataReader, fromPeer);
         }
 
-        public void SendDataOverEvent(object Data, DeliveryMethod Order)
+        public void SendDataOverEvent<T>(T Data, DeliveryMethod Order) where T : class, new()
         {
-            listener.PeerConnectedEvent += peer =>
-            {
-                //  ThomasEngine.Debug.Log("Server " + peer.EndPoint);// Show peer ip 
-                writer.Put((string)Data);             // Put some string
-                peer.Send(writer, Order);                   // Send with reliability
-                writer.Reset();                             //Resets the writer and emptying its array.
-            };
-        }
-        public void SendDataOverEvent(float Data1, float Data2, float Data3, DeliveryMethod Order)
-        {
-            listener.PeerConnectedEvent += peer =>
-            {
-                //  ThomasEngine.Debug.Log("Server " + peer.EndPoint);// Show peer ip 
-                writer.Put(Data1);
-                writer.Put(Data2);
-                writer.Put(Data3);
-                peer.Send(writer, Order);                   // Send with reliability
-                writer.Reset();                             //Resets the writer and emptying its array.
-            };
+            listener.PeerConnectedEvent += peer => netPacketProcessor.Send<T>(peer, Data, Order);
         }
 
+
+        public void SubscribeDataType<T>(Action<T, NetPeer> onReceive) where T : class, new()
+        {
+            netPacketProcessor.SubscribeReusable<T, NetPeer>(onReceive);
+        }
+
+        
     }
 }
