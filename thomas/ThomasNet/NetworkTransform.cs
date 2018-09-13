07312d64449cc482d1efcd0d3f5a8c4a4d1cd08b
Kimpai <kenarsson@hotmail.se>
2018-09-13 10:58:41 +0200

    changes to transform

--- thomas/ThomasNet/NetworkTransform.cs
+++ thomas/ThomasNet/NetworkTransform.cs
@@ -23,24 +23,24 @@ namespace ThomasEngine.Network
         public override void Read(NetPacketReader reader)
         {
             Vector3 pos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-            pos.x += transform.position.x - reader.GetFloat();
-            pos.y += transform.position.y - reader.GetFloat();
-            pos.z += transform.position.z - reader.GetFloat();
+            pos.x += (reader.GetFloat() - transform.position.x);
+            pos.y += (reader.GetFloat() - transform.position.y);
+            pos.z += (reader.GetFloat() - transform.position.z);
 
             transform.position = pos;
 
             Quaternion rot = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
-            rot.x += transform.rotation.x - reader.GetFloat();
-            rot.y += transform.rotation.y - reader.GetFloat();
-            rot.z += transform.rotation.z - reader.GetFloat();
-            rot.w += transform.rotation.w - reader.GetFloat();
+            rot.x += (reader.GetFloat() - transform.rotation.x);
+            rot.y += (reader.GetFloat() - transform.rotation.y);
+            rot.z += (reader.GetFloat() - transform.rotation.z);
+            rot.w += (reader.GetFloat() - transform.rotation.w);
 
             transform.rotation = rot;
 
             Vector3 scale = new Vector3(transform.scale.x, transform.scale.y, transform.scale.z);
-            scale.x += transform.scale.x - reader.GetFloat();
-            scale.y += transform.scale.y - reader.GetFloat();
-            scale.z += transform.scale.z - reader.GetFloat();
+            scale.x += (reader.GetFloat() - transform.scale.x);
+            scale.y += (reader.GetFloat() - transform.scale.y);
+            scale.z += (reader.GetFloat() - transform.scale.z);
 
             transform.scale = scale;
         }
