3c08ea90947c9d714e3439b13b9ba884b1a7dcb9
Vulkan <gustav.ekelund@hotmail.com>
2018-09-14 13:31:14 +0200

    Only delete selected GO

--- thomas/ThomasEditor/MainWindow.xaml.cs
+++ thomas/ThomasEditor/MainWindow.xaml.cs
@@ -229,9 +229,9 @@ namespace ThomasEditor
             for(int i=0; i < ThomasWrapper.Selection.Count; i++)
             {
                 GameObject gObj = ThomasWrapper.Selection.op_Subscript(i);
+                ThomasWrapper.Selection.UnSelectGameObject(gObj);
                 gObj.Destroy();
-                //ThomasWrapper.SelectedGameObjects.RemoveAt(i);
-               i--;
+                //i--;
             }
         }
 
