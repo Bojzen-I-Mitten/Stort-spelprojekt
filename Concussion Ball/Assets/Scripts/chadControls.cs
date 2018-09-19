a8eaaffe1d27ef61d2b5dd02537fb76c7da7d6c3
Nick Wedzinga <oxay@live.com>
2018-09-19 14:34:54 +0200

    Minor changes, preparing for merge

--- Concussion Ball/Assets/Scripts/chadControls.cs
+++ Concussion Ball/Assets/Scripts/chadControls.cs
@@ -16,6 +16,7 @@ namespace ThomasEditor
     {
         public int speed { get; set; } = 5;
         public float force { get; set; } = 5;
+        public int rotationSpeed { get; set; } = 1;
 
         Rigidbody rBody;
 
@@ -34,13 +35,19 @@ namespace ThomasEditor
         IEnumerator JumpingCoroutine()
         {
             jumpDelay = false;
+
             rBody.ApplyCentralImpulseForce(new Vector3(0.0f, force, 0.0f));
             yield return new WaitForSeconds(1.0f);
+
+            if (tackling)
+            {
+                //transform.RotateByAxis(new Vector3(1.0f, 0.0f, 0.0f), -45.0f);
+                tackling = false;
+            }
             jumpDelay = true;
+            jumping = false;
             movingForward = false;
             movingBackward = false;
-            tackling = false;
-            jumping = false;
         }
 
         public override void Update()
@@ -62,11 +69,16 @@ namespace ThomasEditor
             {
                 tackling = true;
                 StartCoroutine(JumpingCoroutine());
+                transform.Rotate(0.0f, -0.5f, 0.0f);
             }
+
+            //TESTING KEY
+            //if(Input.GetKeyDown(Input.Keys.T))
+            //    transform.Rotate(0.0f, -0.5f, 0.0f);
 
             //Set double speed for tackling
             if (tackling)
-                transform.position += transform.forward * speed * 2 * Time.DeltaTime;
+            transform.position += new Vector3(transform.forward.x, 0, transform.forward.z) * speed * 2 * Time.DeltaTime;
 
             //If character is in air and player not pressing W, should still move forward, can't stop mid air
             if (movingForward && !Input.GetKey(Input.Keys.W) && !tackling)
@@ -81,26 +93,26 @@ namespace ThomasEditor
             {
                 transform.position -= transform.forward * speed * Time.DeltaTime;
                 if (Input.GetKey(Input.Keys.A))
-                    transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), -0.5f * Time.DeltaTime);
+                    transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), -rotationSpeed * Time.DeltaTime);
                 if (Input.GetKey(Input.Keys.D))
-                    transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), 0.5f * Time.DeltaTime);
+                    transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), rotationSpeed * Time.DeltaTime);
             }
             //Same, but for forward
             if (Input.GetKey(Input.Keys.W) && !movingBackward && !tackling)
             {
                 transform.position += transform.forward * speed * Time.DeltaTime;
                 if (Input.GetKey(Input.Keys.A))
-                    transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), 0.5f * Time.DeltaTime);
+                    transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), rotationSpeed * Time.DeltaTime);
                 if (Input.GetKey(Input.Keys.D))
-                    transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), -0.5f * Time.DeltaTime);
+                    transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), -rotationSpeed * Time.DeltaTime);
             }
             //If character is standing still
             if(!Input.GetKey(Input.Keys.W) && !Input.GetKey(Input.Keys.S))
             {
                 if (Input.GetKey(Input.Keys.A))
-                    transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), 0.5f * Time.DeltaTime);
+                    transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), rotationSpeed * Time.DeltaTime);
                 if (Input.GetKey(Input.Keys.D))
-                    transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), -0.5f * Time.DeltaTime);
+                    transform.RotateByAxis(new Vector3(0.0f, 1.0f, 0.0f), -rotationSpeed * Time.DeltaTime);
             }
         }
     }
