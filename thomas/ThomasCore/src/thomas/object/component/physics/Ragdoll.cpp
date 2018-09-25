2e7fc2fe209f4313cf898abc32b93758950e02f9
Unknown <deankulenovic@gmail.com>
2018-09-25 15:58:12 +0200

    Added support to add boundingboxes between capsules, something is wrong
    dosent fully work

--- thomas/ThomasCore/src/thomas/object/component/physics/Ragdoll.cpp
+++ thomas/ThomasCore/src/thomas/object/component/physics/Ragdoll.cpp
@@ -5,11 +5,67 @@
 #include "../../../Common.h"
 #include <stdint.h>
 #include "../../../editor/gizmos/Gizmos.h"
-namespace thomas {
-	namespace object {
-		namespace component {
 
-			btRigidBody * Ragdoll::localCreateRigidBody(btScalar mass, const btTransform & startTransform, btCollisionShape * shape)
+namespace thomas 
+{
+	namespace object 
+	{
+		namespace component 
+		{
+			const float RADIUS = 30.f;
+
+			Ragdoll::Ragdoll()
+			{
+				m_bodies[0] = nullptr;
+
+				//init();
+			}
+
+			Ragdoll::~Ragdoll()
+			{
+			}
+
+			void Ragdoll::Update()
+			{
+		/*		btTransform trans;
+				m_bodies[0]->getMotionState()->getWorldTransform(trans);
+
+				math::Quaternion rotation = (math::Quaternion)trans.getRotation();
+				math::Vector3 pos = (math::Vector3)trans.getOrigin();
+				m_gameObject->m_transform->SetPosition(pos);
+				m_gameObject->m_transform->SetRotation(rotation);
+				m_gameObject->m_transform->SetDirty(true);
+				*/
+			}
+
+			void Ragdoll::Awake()
+			{
+				Init();
+				//			m_shapes[BodyPart_Chest] = new btCapsuleShape(btScalar(0.15), btScalar(0.20));
+				//				btTransform trans;
+
+				//				math::Vector3 pos = m_gameObject->m_transform->GetPosition();
+				//			math::Quaternion rot = m_gameObject->m_transform->GetRotation();
+				//			trans.setOrigin((btVector3&)pos);
+				//			trans.setRotation((btQuaternion&)rot);
+				//			m_bodies[BodyPart_Chest] = localCreateRigidBody(btScalar(1.), trans, m_shapes[BodyPart_Chest]);
+
+			}
+			void Ragdoll::OnDrawGizmos()
+			{
+				if (m_bodies[BodyParts::BodyPart_Chest] != nullptr)
+				{
+					btTransform t;
+					m_bodies[BodyPart_Chest]->getMotionState()->getWorldTransform(t);
+					math::Matrix m = math::CreateMatrix(Physics::ToSimple(t.getOrigin()), Physics::ToSimple(t.getRotation()), m_gameObject->m_transform->GetScale());
+
+					editor::Gizmos::SetColor(math::Color(0, 1, 0));
+					editor::Gizmos::SetMatrix(m);
+					editor::Gizmos::DrawBoundingCapsule(DirectX::SimpleMath::Vector3(0, 0, 0), RADIUS, m_lengths[0]);
+				}
+			}
+
+			btRigidBody * Ragdoll::LocalCreateRigidBody(btScalar mass, const btTransform & startTransform, btCollisionShape * shape)
 			{
 				bool isDynamic = (mass != 0.f);
 
@@ -27,7 +83,7 @@ namespace thomas {
 				return body;
 			}
 
-			uint32_t hash(const char *str)
+			unsigned int Ragdoll::Hash(const char *str)
 			{
 				unsigned long hash = 5381;
 				int c;
@@ -38,9 +94,8 @@ namespace thomas {
 				return hash;
 			}
 
-			btCapsuleShape* prepareCapsulTransform(const math::Matrix& fromWorld, const math::Matrix& toWorld, float radius, btTransform &outTrans)
+			btCapsuleShape* Ragdoll::PrepareCapsulTransform(const math::Matrix& fromWorld, const math::Matrix& toWorld, float radius, btTransform &outTrans)
 			{
-
 				math::Vector3 m_bone_pos = fromWorld.Translation();
 				math::Vector3 m_boneNext_pos = toWorld.Translation();
 				math::Vector3 m_up = m_boneNext_pos - m_bone_pos;
@@ -48,29 +103,24 @@ namespace thomas {
 				float length = m_up.Length();
 				math::Vector3 m_pos = m_bone_pos + m_up * (length * 0.5f);
 
-				outTrans.setOrigin(reinterpret_cast<btVector3&>(m_pos));
-
 				math::Quaternion q(m_up, 0.f);
+				outTrans.setOrigin(reinterpret_cast<btVector3&>(m_pos));
 				outTrans.setRotation(reinterpret_cast<btQuaternion&>(q));
-
+				
+				m_lengths.push_back(length);
 				return new btCapsuleShape(radius, length);
 			}
 
 			const static std::string boneNames[Ragdoll::BodyParts::BodyParts_Amount]
 			{
-				"hips",
-				"spine5",
-				"<Name>",
-				"<Name>",
-				"<Name>",
-
+				"mixamorig:Hips",
+				"mixamorig:Head"
 			};
 
-
 			/* Identifies all skeleton bone indices
-			*/
-			/*
-			void Ragdoll::initSkeleton() {
+			*/	
+			void Ragdoll::InitSkeleton() 
+			{
 				// Try fetch skeleton
 				thomas::object::GameObject* gObj = m_gameObject;
 				assert(gObj);
@@ -84,11 +134,11 @@ namespace thomas {
 
 
 				bool foundBones = true;
-				m_boneIndex = std::vector<uint32_t>(m_tree->boneCount);
+				m_boneIndex = std::vector<uint32_t>(m_tree->boneCount());
 				uint32_t i = 0;
 				for (; i < BodyParts::BodyParts_Amount; i++)
 				{
-					bool found = m_tree->getBoneIndex(hash(boneNames[i].c_str()), m_boneIndex[i]);
+					bool found = m_tree->getBoneIndex(Hash(boneNames[i].c_str()), m_boneIndex[i]);
 					foundBones &= found;
 #ifdef _DEBUG
 					if (!found) {
@@ -102,10 +152,11 @@ namespace thomas {
 					return;
 				}
 			}
-			*/
-			/*
-			void Ragdoll::init()
+				
+			void Ragdoll::Init()
 			{
+				InitSkeleton();
+
 				if (!m_tree) 
 					return;
 
@@ -116,15 +167,15 @@ namespace thomas {
 				// Fetch bone transform in object space
 
 				math::Matrix boneArr[BodyParts::BodyParts_Amount];
-				for (unsigned int i = 0; i < BodyParts::BodyParts_Amount; i++) {
+				for (unsigned int i = 0; i < BodyParts::BodyParts_Amount; i++)
 					boneArr[i] = m_tree->getBoneMatrix(m_boneIndex[i]) * world;
-				}
 
-				constexpr float chestRad = 0.3f;
+				
+			//	constexpr float chestRad = 5;
 
 				btTransform transform;
-				m_shapes[BodyPart_Chest] = prepareCapsulTransform(boneArr[0], boneArr[1], chestRad, transform);
-				m_bodies[BodyPart_Chest] = localCreateRigidBody(btScalar(0.), transform, m_shapes[BodyPart_Chest]);
+				m_shapes[BodyPart_Chest] = PrepareCapsulTransform(boneArr[0], boneArr[1], RADIUS, transform);
+				m_bodies[BodyPart_Chest] = LocalCreateRigidBody(btScalar(0), transform, m_shapes[BodyPart_Chest]);
 
 				//	thomas::Physics::s_world;
 				//	m_shapes[]
@@ -140,49 +191,6 @@ namespace thomas {
 				//	m_shapes[BodyPart_Right_UpperArm] = new btCapsuleShape(btScalar(scale_ragdoll*0.10), btScalar(scale_ragdoll*0.20));
 				//	m_shapes[BodyPart_Right_UpperLeg] = new btCapsuleShape(btScalar(scale_ragdoll*0.10), btScalar(scale_ragdoll*0.20));
 				//  moving the capsuleposition to the GameObjects position(wrong should be moved to the joints position)
-
-
-			}
-			*/
-			Ragdoll::Ragdoll()
-			{
-//				init();
-			
-			}
-
-			Ragdoll::~Ragdoll()
-			{
-
-			}
-
-			void Ragdoll::Update()
-			{
-
-				
-				btTransform trans;
-				m_bodies[0]->getMotionState()->getWorldTransform(trans);
-
-				math::Quaternion rotation = (math::Quaternion)trans.getRotation();
-				math::Vector3 pos = (math::Vector3)trans.getOrigin();
-				m_gameObject->m_transform->SetPosition(pos);
-				m_gameObject->m_transform->SetRotation(rotation);
-				m_gameObject->m_transform->SetDirty(true);
-				
-			}
-			void Ragdoll::Awake()
-			{
-				
-				m_shapes[BodyPart_Chest] = new btCapsuleShape(btScalar(0.15), btScalar(0.20));
-				btTransform trans;
-
-				math::Vector3 pos = m_gameObject->m_transform->GetPosition();
-				math::Quaternion rot = m_gameObject->m_transform->GetRotation();
-
-
-				trans.setOrigin((btVector3&)pos);
-				trans.setRotation((btQuaternion&)rot);
-				m_bodies[BodyPart_Chest] = localCreateRigidBody(btScalar(1.), trans, m_shapes[BodyPart_Chest]);
-				
 			}
 		}
 	}
--- thomas/ThomasCore/src/thomas/object/component/physics/Ragdoll.h
+++ thomas/ThomasCore/src/thomas/object/component/physics/Ragdoll.h
@@ -3,8 +3,7 @@
 #include "../Component.h"
 #include "../../../Physics.h"
 #include "Collider.h"
-
-
+#include "../../../graphics/animation/IBlendTree.h"
 
 namespace thomas
 {
@@ -17,10 +16,9 @@ namespace thomas
 
 			public:
 				enum BodyParts
-				{
-				
+				{	
 					BodyPart_Chest,
-					
+		
 					BodyPart_Left_UpperArm,
 					BodyPart_Left_LowerArm,
 
@@ -32,14 +30,13 @@ namespace thomas
 
 					BodyPart_Right_UpperLeg,
 					BodyPart_Right_LowerLeg,
-					BodyParts_Amount
-					
+					BodyParts_Amount		
 				};
 
 				enum Joint
 				{
 					Joint_Neck,
-					joint_pelvis,
+					Joint_pelvis,
 
 					Joint_Left_Arm,
 					Joint_Right_Arm,
@@ -48,25 +45,28 @@ namespace thomas
 					Joint_Right_Leg,
 					Joint_Amount
 				};
+				
+			public:
+				Ragdoll();
+				~Ragdoll();
+				virtual void Update() override;
+				virtual void Awake() override;
+				virtual void OnDrawGizmos() override;
+
+			private:
+				void Init();
+				void InitSkeleton();
+				unsigned int Hash(const char *str);
+				btRigidBody* LocalCreateRigidBody(btScalar mass, const btTransform& startTransform, btCollisionShape* shape);
+				btCapsuleShape* PrepareCapsulTransform(const math::Matrix& fromWorld, const math::Matrix& toWorld, float radius, btTransform &outTrans);
+
 			private:
 				btCollisionShape* m_shapes[BodyParts_Amount];
 				btRigidBody* m_bodies[BodyParts_Amount];
 				btTypedConstraint* m_joints[Joint_Amount];
-				
-
-				btRigidBody* localCreateRigidBody(btScalar mass, const btTransform& startTransform, btCollisionShape* shape);
-
-//				thomas::graphics::animation::IBlendTree* m_tree;
+				thomas::graphics::animation::IBlendTree* m_tree;
 				std::vector<uint32_t> m_boneIndex;
-
-				void init();
-				void initSkeleton();
-
-			public:
-				Ragdoll(); // tar emot objectets position och en scale på figuren. 
-				~Ragdoll();
-				virtual void Update() override;
-				virtual void Awake() override;
+				std::vector<float> m_lengths;
 			};
 
 		}
