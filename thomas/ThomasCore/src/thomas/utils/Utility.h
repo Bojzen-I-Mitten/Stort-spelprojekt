effca4b6100e66bd61c9c655a3b78645a735bf7d
Mattias <mattias-fredriksson91@hotmail.se>
2018-10-01 13:47:12 +0200

    Added Allocator

--- thomas/ThomasCore/src/thomas/utils/Utility.h
+++ thomas/ThomasCore/src/thomas/utils/Utility.h
@@ -31,5 +31,70 @@ namespace thomas {
 		inline bool matchFlags(uint32_t value, uint32_t flags) {
 			return (value & flags) == flags;
 		}
+		inline void* alignForward(void* address, uint8_t alignment)
+		{
+			return (void*)((reinterpret_cast<uintptr_t>(address) + static_cast<uintptr_t>(alignment - 1)) & static_cast<uintptr_t>(~(alignment - 1)));
+		}
+
+		inline const void* alignForward(const void* address, uint8_t alignment)
+		{
+			return (void*)((reinterpret_cast<uintptr_t>(address) + static_cast<uintptr_t>(alignment - 1)) & static_cast<uintptr_t>(~(alignment - 1)));
+		}
+
+		inline void* alignBackward(void* address, uint8_t alignment)
+		{
+			return (void*)(reinterpret_cast<uintptr_t>(address)& static_cast<uintptr_t>(~(alignment - 1)));
+		}
+
+		inline const void* alignBackward(const void* address, uint8_t alignment)
+		{
+			return (void*)(reinterpret_cast<uintptr_t>(address)& static_cast<uintptr_t>(~(alignment - 1)));
+		}
+
+		inline uint8_t alignForwardAdjustment(const void* address, uint8_t alignment)
+		{
+			uint8_t adjustment = alignment - (reinterpret_cast<uintptr_t>(address)& static_cast<uintptr_t>(alignment - 1));
+
+			if (adjustment == alignment)
+				return 0; //already aligned
+
+			return adjustment;
+		}
+		/*
+		inline uint8_t alignForwardAdjustmentWithHeader(const void* address, uint8_t alignment, uint8_t header_size)
+		{
+			uint8_t adjustment = alignForwardAdjustment(address, alignment);
+			uint8_t needed_space = header_size;
+			if(adjustment < needed_space)
+			{
+				needed_space -= adjustment;
+				//Increase adjustment to fit header
+				adjustment += alignment * (needed_space / alignment);
+				if(needed_space % alignment > 0)
+					adjustment += alignment;
+			}
+			return adjustment;
+		}
+		*/
+		template<typename T>
+		inline uint8_t alignForwardAdjustmentWithHeader(const void* address, uint8_t alignment)
+		{
+			if (__alignof(T) > alignment)
+				alignment = __alignof(T);
+
+			uint8_t adjustment = sizeof(T) + alignForwardAdjustment(pointer_math::add(address, sizeof(T)), alignment);
+
+			return adjustment;
+		}
+
+		inline uint8_t alignBackwardAdjustment(const void* address, uint8_t alignment)
+		{
+			uint8_t adjustment = reinterpret_cast<uintptr_t>(address)& static_cast<uintptr_t>(alignment - 1);
+
+			if (adjustment == alignment)
+				return 0; //already aligned
+
+			return adjustment;
+		}
 	}
 }
---
+++ thomas/ThomasCore/src/thomas/utils/allocator/LinearAllocator.cpp
@@ -0,0 +1,46 @@
+#include "LinearAllocator.h" 
+#include "../Utility.h"
+
+namespace thomas {
+	namespace utility {
+		namespace allocator {
+
+
+			LinearAllocator::LinearAllocator(size_t size, void* start) 
+				: Allocator(size, start), _current_pos(start) 
+			{ 
+				assert(size > 0); 
+			}
+
+			LinearAllocator::~LinearAllocator() { _current_pos = nullptr; }
+
+			void* LinearAllocator::allocate(size_t size, uint8_t alignment)
+			{
+				assert(size != 0);
+				uint8_t adjustment = alignForwardAdjustment(_current_pos, alignment);
+
+				if (_used_memory + adjustment + size > _size) return nullptr;
+
+				uintptr_t  aligned_address = (uintptr_t)_current_pos + adjustment;
+				_current_pos = (void*)(aligned_address + size);
+				_used_memory += size + adjustment;
+				_num_allocations++;
+
+				return (void*)aligned_address;
+			}
+
+			void LinearAllocator::deallocate(void* p)
+			{
+				assert(false && "Use clear() instead");
+			}
+
+			void LinearAllocator::clear()
+			{
+				_num_allocations = 0;
+				_used_memory = 0;
+				_current_pos = _start;
+			}
+
+		}
+	}
+}
