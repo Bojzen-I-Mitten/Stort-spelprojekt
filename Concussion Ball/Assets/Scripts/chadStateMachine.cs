4c35ce3adcaef6c2bf6b9c9f7c06c33cc1a66b95
Nick Wedzinga <oxay@live.com>
2018-10-08 15:58:14 +0200

    Same basic state machine bois all up in there.

--- Concussion Ball/Assets/Scripts/chadStateMachine.cs
+++ Concussion Ball/Assets/Scripts/chadStateMachine.cs
@@ -16,21 +16,63 @@ namespace ThomasEditor
         TURNING = 3,    // turning has to call different functions depending on if previous state was idle or walking/running
         THROWING = 4,   // player throws ball / power-up, not all power-ups activate this state
         RAGDOLL = 5,    // user got tackled / hit by a power-up
-        JUMP = 6,
+        JUMPING = 6,
+    };
 
-    }
-
-    public class ChadStateMachine : NetworkComponent
+    public class ChadStateMachine : ScriptComponent
     {
-        //public int testBoi { get; set; } = 0;
+        State m_state;
+        Rigidbody rb;
+        float xStep = 0.0f;
+
         public override void Start()
         {
+            m_state = State.IDLE;
+            xStep = Input.GetMouseX() * Time.ActualDeltaTime;
 
+            rb = gameObject.GetComponent<Rigidbody>();
         }
 
         public override void Update()
         {
+            //Debug.Log("Updating..");
+            Input.SetMouseMode(Input.MouseMode.POSITION_RELATIVE);
+            switch (m_state)
+            {
+                case State.IDLE:
+                    // stop all animations
+                    // check if W or SPACE or LMB is pressed or collision check
+                    if (Input.GetMouseButton(Input.MouseButtons.LEFT))
+                    {
+                        Debug.Log("Changed state to THROWING");
+                        m_state = State.THROWING;
+                    }
+                    //else if (rb.isCollidingWithPowerUpOrChad())
+                    //    m_state = State.RAGDOLL;
+                    else if (Input.GetKey(Input.Keys.Space))
+                    {
+                        Debug.Log("Changed state to JUMPING");
+                        m_state = State.JUMPING;
+                    }
+                    else if (Input.GetKey(Input.Keys.W))
+                    {
+                        m_state = State.WALKING;
+                        Debug.Log("Changed state to WALKING");
+                    }
+                    else if (Input.GetMouseX() * Time.ActualDeltaTime != xStep)
+                        m_state = State.TURNING;
+                    break;
+                case State.WALKING:
+                    // call chadMovement bla bla
+                    Debug.Log("Walking");
+                    if(!Input.GetKey(Input.Keys.W))
+                    {
+                        m_state = State.IDLE;
+                        Debug.Log("Changed state to IDLE");
+                    }
 
+                    break;
+            }
         }
     }
 }
