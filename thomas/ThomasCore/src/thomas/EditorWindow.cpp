75914ffe419d266253fab03fdccbee6bd4f183d2
Jakob Ahrneteg <jakob.ahrneteg@gmail.com>
2018-10-09 16:00:35 +0200

    Started integrating nuklear as GUI library for game view

--- thomas/ThomasCore/src/thomas/EditorWindow.cpp
+++ thomas/ThomasCore/src/thomas/EditorWindow.cpp
@@ -5,16 +5,40 @@
 #include <imgui\imgui_impl_dx11.h>
 #include <imgui\ImGuizmo.h>
 
+// Nuklear
+#define MAX_VERTEX_BUFFER 512 * 1024
+#define MAX_INDEX_BUFFER 128 * 1024
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_D3D11_IMPLEMENTATION
+#include <nuklear/nuklear.h>
+#include <nuklear/nuklear_d3d11.h>
+
+struct nk_context* m_nkContext;
+
 namespace thomas
 {
 	EditorWindow::EditorWindow(HINSTANCE hInstance, int nCmdShow, const LONG & width, const LONG & height, const LPCSTR & title) : 
 		Window(hInstance, nCmdShow, width, height, title)
 	{
-
 	}
 
 	EditorWindow::EditorWindow(HWND hWnd) : Window(hWnd)
 	{
+		m_nkContext = nk_d3d11_init(utils::D3D::Instance()->GetDevice(), utils::D3D::Instance()->GetDeviceContext(), 
+									0, 0, MAX_VERTEX_BUFFER, MAX_INDEX_BUFFER);
+
+		struct nk_font_atlas* atlas;
+		nk_d3d11_font_stash_begin(&atlas);
+		nk_d3d11_font_stash_end();
+
 		ImGui_ImplDX11_Init(hWnd, utils::D3D::Instance()->GetDevice(), utils::D3D::Instance()->GetDeviceContext());
 	}
 
