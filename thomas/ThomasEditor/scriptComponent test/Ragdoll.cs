14de4879585c6473c8951f6cd06a987394acfb4c
Unknown <deankulenovic@gmail.com>
2018-10-10 10:55:15 +0200

    more work on ragdolls

--- thomas/ThomasEditor/scriptComponent test/Ragdoll.cs
+++ thomas/ThomasEditor/scriptComponent test/Ragdoll.cs
@@ -9,16 +9,64 @@ using ThomasEngine;
 namespace ThomasEditor
 {
     public class Ragdoll : ScriptComponent
-    {               
+    {
+        public string Spine { get; set; }
+        public string Hips { get; set; }
+        public Vector3 displaydata { get; set; }
         public override void Start()
         {
+          
            
-             gameObject.AddComponent<LightComponent>();
-            //    GameObject RagdollCharacter = new GameObject("RagdollCharacter");
-            //    RenderSkinnedComponent Skeleton =  RagdollCharacter.AddComponent<RenderSkinnedComponent>(); 
+            //Hips
+            GameObject G_Hips = new GameObject("Hips");
+            G_Hips.transform.SetParent(gameObject.transform);
             
+            BoneTransformComponent B_Hips = G_Hips.AddComponent<BoneTransformComponent>();
+            B_Hips.BoneName = Hips;
+            B_Hips.AnimatedObject = gameObject;
+
+
+
+
+            //Spine
+        //    Matrix test = gameObject.GetComponent<RenderSkinnedComponent>().GetBoneMatrix(2);
+       /*     Vector3 test1;
+            Quaternion test2;
+            Vector3 test3;
+            test.Decompose(out test1, out test2, out test3);
+            displaydata = test3;
+
+            Vector3 bone1 =  B_Hips.transform.localPosition;
+            Vector3 bone2 = displaydata;
+            Vector3 up = bone2 - bone1;
+            float length = up.Length();
+            if (length != 0)
+                up /= length; // Normalize
+            Vector3 Pos = bone1 + up * (length * 0.5f);
+
+    */
+
         }
 
+       
+
+       
+    void initSkeleton(RenderSkinnedComponent Model)
+        {
+         
+        }
+        int hash(string data)
+        {
+            int hashcode = 5381;
+
+            for(int i= 0;i<data.Length;i++)
+            {
+                hashcode = ((hashcode << 5) + hashcode) + (int)data[i];
+            }
+
+
+            return hashcode;
+        }
         public override void Update()
         {
         }
@@ -32,7 +80,7 @@ namespace ThomasEditor
         }
         public override void OnDisable()
         {
-            
+           
         }
 
     }
