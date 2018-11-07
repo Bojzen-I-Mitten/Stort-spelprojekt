01465b37dcb0cad61e34093db1b291148f48d7ef
Albin <albinsahlbom@hotmail.com>
2018-11-07 12:29:18 +0100

    Fixed item picked up text.

--- Concussion Ball/Assets/Scripts/Chad/chadControls.cs
+++ Concussion Ball/Assets/Scripts/Chad/chadControls.cs
@@ -166,6 +166,16 @@ public class ChadControls : NetworkComponent
         }
         if (Input.GetKeyDown(Input.Keys.K))
             gameObject.GetComponent<NetworkPlayer>().Reset();
+
+        if (PickedUpObject)
+        {
+            ChadHud.Instance.ShowHeldObjectText(PickedUpObject.gameObject.Name);
+        }
+        else
+        {
+            ChadHud.Instance.HideHeldObjectText();
+            ChadHud.Instance.DeactivateAimHUD();
+        }
     }
 
     private void EnableRagdoll()
@@ -195,7 +205,6 @@ public class ChadControls : NetworkComponent
     {
         if (isOwner && PickedUpObject && PickedUpObject.DropOnRagdoll)
         {
-            ChadHud.Instance.HideHeldObjectText();
             if (typeof(Powerup).IsAssignableFrom(PickedUpObject.GetType()))
             {
                 Debug.Log("remove");
@@ -519,9 +528,6 @@ public class ChadControls : NetworkComponent
         StopCoroutine(Ragdolling);
         CurrentVelocity = Vector2.Zero;
         ResetCamera();
-        //ChadHud.Instance.HideHeldObjectText();
-        //ChadHud.Instance.DeactivateChargeBar();
-        //ChadHud.Instance.DeactivateCrosshair();
         if (PickedUpObject)
         {
             PickedUpObject.Drop();
@@ -622,7 +628,6 @@ public class ChadControls : NetworkComponent
     private void ThrowObject(Vector3 camPos, Vector3 direction)
     {
         PickedUpObject.Throw(camPos, direction);
-        ChadHud.Instance.HideHeldObjectText();
     }
 
     public void RPCPickup(int id)
@@ -696,8 +701,6 @@ public class ChadControls : NetworkComponent
                     TakeOwnership(pickupable.gameObject);
                     SendRPC("RPCPickup", pickupable.ID);
                     RPCPickup(pickupable.ID);
-                    
-                    ChadHud.Instance.ShowHeldObjectText(pickupable.gameObject.Name);
                 }
             }
             ChadControls otherChad = collider.gameObject.GetComponent<ChadControls>();
