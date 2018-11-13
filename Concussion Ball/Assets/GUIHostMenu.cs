c2c85245969dcbb240544fee58c4ad2538a7b624
Albin <albinsahlbom@hotmail.com>
2018-11-13 15:27:45 +0100

    Working on names for the teams.

--- Concussion Ball/Assets/GUIHostMenu.cs
+++ Concussion Ball/Assets/GUIHostMenu.cs
@@ -9,20 +9,42 @@ public class GUIHostMenu : ScriptComponent
 
     public Texture2D ColorSliderTexture { get; set; }
     public Texture2D SliderKnobTexture { get; set; }
+    public Texture2D newBG { get; set; }
+    public Texture2D TextBoxTexture { get; set; }
+    public Texture2D WhiteBoxTexture { get; set; }
     public Font Font { get; set; }
     public int NrTeams { get; set; } = 2;
 
-    Text SelectTeamColor;
-    List<Text> Teams = new List<Text>();
-    List<Image> ColorSliders = new List<Image>();
-    List<Image> SliderKnob = new List<Image>();
-    List<Image> SelectTiles = new List<Image>();
+    //public Vector2 TextBox1Pos { get; set; } = Vector2.Zero;
+    //public Vector2 TextBox1Scale { get; set; } = Vector2.One;
+    //public Vector4 HostBGColor { get; set; } = Vector4.One;
+
+    #region Host Setings
+    Image HostBg;
+    #endregion
+
+    #region Teams
+    Text Team1;
+    Text Team2;
+
+    Image Team1TextBox;
+    Image Team1ColorSlider;
+    Image Team1SliderKnob;
+    Image Team1BG;
+
+    Image Team2TextBox;
+    Image Team2ColorSlider;
+    Image Team2SliderKnob;
+    Image Team2BG;
+    Image BG;
 
     bool btnDown;
+    string _team1 = "Team 1";
+    string _team2 = "Team 2";
+    #endregion
 
     public override void Start()
     {
-        
     }
 
     public override void Awake()
@@ -32,40 +54,86 @@ public class GUIHostMenu : ScriptComponent
 
         btnDown = false;
 
-        SelectTeamColor = Canvas.Add("Select team color!");
-        SelectTeamColor.color = Color.Black;
-        SelectTeamColor.position = new Vector2(0.3f, 0.1f);
-        SelectTeamColor.origin = new Vector2(0.5f);
-        SelectTeamColor.font = Font;
+        BG = Canvas.Add(newBG);
+        BG.depth = 0.6f;
+
+        #region Host Setings
+
+        if(WhiteBoxTexture != null)
+        {
+            HostBg = Canvas.Add(WhiteBoxTexture);
+            HostBg.color = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+            HostBg.scale = new Vector2(6.4f, 1.44f); ;
+        }
+
+        #endregion
+
+        #region Teams
+        if (WhiteBoxTexture != null)
+        {
+            Team1BG = Canvas.Add(WhiteBoxTexture);
+            Team1BG.position = new Vector2(0.0f, 0.4f);
+            Team1BG.scale = new Vector2(2, 2.2f);
+            Team1BG.color = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+            Team1BG.depth = 0.5f;
+
+            Team2BG = Canvas.Add(WhiteBoxTexture);
+            Team2BG.position = new Vector2(0.67f, 0.4f);
+            Team2BG.scale = new Vector2(2.2f, 2.4f);
+            Team2BG.color = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+            Team2BG.depth = 0.5f;
+        }
+
+        Team1 = Canvas.Add(_team1);
+        Team1.position = new Vector2(0.02f, 0.4f);
+        Team1.color = Color.Black;
+        Team1.font = Font;
+
+        Team2 = Canvas.Add(_team2);
+        Team2.position = new Vector2(0.69f, 0.4f);
+        Team2.color = Color.Black;
+        Team2.font = Font;
+
+        if (TextBoxTexture != null)
+        {
+            Team1TextBox = Canvas.Add(TextBoxTexture);
+            Team1TextBox.position = new Vector2(0.01f, 0.39f);
+            Team1TextBox.scale = new Vector2(0.9f, 1f);
+            Team1TextBox.interactable = true;
+
+            Team2TextBox = Canvas.Add(TextBoxTexture);
+            Team2TextBox.position = new Vector2(0.68f, 0.39f);
+            Team2TextBox.scale = new Vector2(0.9f, 1f);
+            Team2TextBox.interactable = true;
+        }
+
+        Vector2 team1SliderKnobPos = new Vector2(0.05f, 0.7f);
+        Vector2 team2SliderKnobPos = new Vector2(0.70f, 0.7f);
 
         if (ColorSliderTexture != null)
         {
-            for (int team = 0; team < NrTeams; team++)
-            {
-                Text t = Canvas.Add("Team " + (team + 1) + ":");
-                t.position = new Vector2(0.05f, 0.25f + 0.10f * team);
-                t.color = Color.Black;
-                t.font = Font;
-                Teams.Add(t);
+            Team1ColorSlider = Canvas.Add(ColorSliderTexture);
+            Team1ColorSlider.position = team1SliderKnobPos;
+            Team1ColorSlider.interactable = true;
 
-                Image i = Canvas.Add(ColorSliderTexture);
-                i.position = new Vector2(0.05f + 0.65f * team, 0.7f);
-                i.interactable = true;
-                ColorSliders.Add(i);
-            }
+            Team2ColorSlider = Canvas.Add(ColorSliderTexture);
+            Team2ColorSlider.position = team2SliderKnobPos;
+            Team2ColorSlider.interactable = true;
         }
 
         if (SliderKnobTexture != null)
         {
-            for (int team = 0; team < NrTeams; team++)
-            {
-                Image i = Canvas.Add(SliderKnobTexture);
-                i.position = new Vector2(0.05f + 0.65f * team, 0.7f);
-                i.origin = new Vector2(0.5f);
-                i.interactable = true;
-                SliderKnob.Add(i);
-            }
+            Team1SliderKnob = Canvas.Add(SliderKnobTexture);
+            Team1SliderKnob.position = team1SliderKnobPos;
+            Team1SliderKnob.origin = new Vector2(0.5f);
+            Team1SliderKnob.color = HSLColor(0d);
+
+            Team2SliderKnob = Canvas.Add(SliderKnobTexture);
+            Team2SliderKnob.position = team2SliderKnobPos;
+            Team2SliderKnob.origin = new Vector2(0.5f);
+            Team2SliderKnob.color = HSLColor(0d);
         }
+        #endregion
     }
 
     public override void Update()
@@ -75,39 +143,38 @@ public class GUIHostMenu : ScriptComponent
             if (Input.GetMouseButtonDown(Input.MouseButtons.LEFT) || btnDown)
             {
                 btnDown = true;
-                for (int i = 0; i < ColorSliders.Count; i++)
-                {
-                    if(ColorSliders[i].Hovered())
-                        SliderKnob[i].position = new Vector2(Input.GetMouseX() / Canvas.camera.viewport.size.x, SliderKnob[i].position.y);
-                }
+                float hue = 0.0f;
+
+                if (Team1ColorSlider.Hovered())
+                    Team1SliderKnob.position = new Vector2(Input.GetMouseX() / Canvas.camera.viewport.size.x, Team1SliderKnob.position.y);
+                hue = (Team1SliderKnob.position.x - Team1ColorSlider.position.x) / (Team1ColorSlider.size.x / Canvas.camera.viewport.size.x);
+                Team1SliderKnob.color = HSLColor(hue);
+
+                if (Team2ColorSlider.Hovered())
+                    Team2SliderKnob.position = new Vector2(Input.GetMouseX() / Canvas.camera.viewport.size.x, Team2SliderKnob.position.y);
+                hue = (Team2SliderKnob.position.x - Team2ColorSlider.position.x) / (Team2ColorSlider.size.x / Canvas.camera.viewport.size.x);
+                Team2SliderKnob.color = HSLColor(hue);
             }
 
-            if(Input.GetMouseButtonUp(Input.MouseButtons.LEFT))
+            if (Input.GetMouseButtonUp(Input.MouseButtons.LEFT))
+            {
                 btnDown = false;
+                Team1TextBox.color = Color.White;
+                Team2TextBox.color = Color.White;
 
-            //if (Input.GetMouseButtonUp(Input.MouseButtons.LEFT))
-            //{
-            //for (int team = 0; team < NrTeams; team++)
-            //{
-            //    for (int color = 0; color < Colors.Count; color++)
-            //    {
-            //        if ((TeamColors[team][color].Hovered() || color == SelectedColors[team]) && color != SelectedColors[(team+1) % 2])
-            //        {
-            //            SelectedColors[team] = color;
-            //            SelectTiles[team].position = TilePosition(team, color);
-            //        }
-            //    }
-            //}
-            //}
+                if (Team1TextBox.Hovered())
+                { 
+                    Team1TextBox.color = Color.Green;
+                }
+                else if (Team2TextBox.Hovered())
+                {
+                    Team2TextBox.color = Color.Green;
+                }
+            }
         }
     }
 
-    private Vector2 TilePosition(int team, int color)
-    {
-        return new Vector2(0.1f + 0.1f * (color + 1), 0.25f + 0.1f * team);
-    }
-
-    private Vector3 HSLColor(double hue)
+    private Color HSLColor(double hue)
     {
         double saturation = 1.0d;
         double luminosity = 0.5d;
@@ -115,7 +182,7 @@ public class GUIHostMenu : ScriptComponent
         byte r, g, b;
 
         double t1, t2;
-        double th = hue / 6.0d;
+        double th = hue; // /6.0d;
 
         t2 = (luminosity + saturation) - (luminosity * saturation);
         t1 = 2d * luminosity - t2;
@@ -131,7 +198,7 @@ public class GUIHostMenu : ScriptComponent
         r = (byte)Math.Round(tr * 255d);
         g = (byte)Math.Round(tg * 255d);
         b = (byte)Math.Round(tb * 255d);
-        return new Vector3(r, g, b);
+        return new Color(r, g, b);
     }
 
     private double ColorCalc(double c, double t1, double t2)
