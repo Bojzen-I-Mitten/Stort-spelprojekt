256d30267792c60871353c58bfbeb268a379fc68
Albin <albinsahlbom@hotmail.com>
2018-11-16 13:29:29 +0100

    Fixed pos for Select Team GUI

--- Concussion Ball/Assets/Scripts/Camera/GUI/GUISelectTeam.cs
+++ Concussion Ball/Assets/Scripts/Camera/GUI/GUISelectTeam.cs
@@ -6,6 +6,13 @@ public class GUISelectTeam : ScriptComponent
     public Texture2D SelectBox { get; set; }
     public Font TextFont { get; set; }
 
+    public Vector2 Team1TP { get; set; } = new Vector2(0.3f, 0.23f);
+    public Vector2 Team1IP { get; set; } = new Vector2(0.35f, 0.3f);
+    public Vector2 Team2TP { get; set; } = new Vector2(0.6f, 0.23f);
+    public Vector2 Team2IP { get; set; } = new Vector2(0.65f, 0.3f);
+    public Vector2 SpecTP { get; set; } = new Vector2(0.8f, 0.15f);
+    public Vector2 SpecIP { get; set; } = new Vector2(0.8f, 0.23f);
+
     Camera Camera;
     private bool Disabled;
 
@@ -73,6 +80,14 @@ public class GUISelectTeam : ScriptComponent
                 Team2Text.font = TextFont;
                 SpectatorText.font = TextFont;
             }
+
+            //Team1Image.position = Team1IP;
+            //Team1Text.position = Team1TP;
+            //Team2Image.position = Team2IP;
+            //Team2Text.position = Team2TP;
+            //SpectatorImage.position = SpecIP;
+            //SpectatorText.position = SpecTP;
+
         }
     }
 
@@ -81,34 +96,41 @@ public class GUISelectTeam : ScriptComponent
         Canvas = Camera.AddCanvas();
 
         Team1Image = Canvas.Add(SelectBox);
-        Team1Image.position = new Vector2(0.13f, 0.23f);
+        Team1Image.position = new Vector2(0.37f, 0.33f);
+        Team1Image.origin = new Vector2(0.5f);
         Team1Image.interactable = true;
         Team1Image.color = MatchSystem.instance.Teams[TEAM_TYPE.TEAM_1].Color;
 
         Team2Image = Canvas.Add(SelectBox);
-        Team2Image.position = new Vector2(0.25f, 0.23f);
+        Team2Image.position = new Vector2(0.67f, 0.33f);
+        Team2Image.origin = new Vector2(0.5f);
         Team2Image.interactable = true;
         Team2Image.color = MatchSystem.instance.Teams[TEAM_TYPE.TEAM_2].Color;
 
         SpectatorImage = Canvas.Add(SelectBox);
-        SpectatorImage.position = new Vector2(0.37f, 0.23f);
+        SpectatorImage.position = new Vector2(0.85f, 0.23f);
+        SpectatorImage.origin = new Vector2(0.5f);
         SpectatorImage.interactable = true;
         SpectatorImage.color = MatchSystem.instance.Teams[TEAM_TYPE.TEAM_SPECTATOR].Color;
 
         Select = Canvas.Add("Select Team");
-        Select.position = new Vector2(0.21f, 0.11f);
+        Select.position = new Vector2(0.5f, 0.1f);
+        Select.origin = new Vector2(0.5f);
         Select.color = Color.FloralWhite;
 
         Team1Text = Canvas.Add(MatchSystem.instance.Teams[TEAM_TYPE.TEAM_1].Name);
-        Team1Text.position = new Vector2(0.10f, 0.18f);
+        Team1Text.position = new Vector2(0.36f, 0.25f);
+        Team1Text.origin = new Vector2(0.5f);
         Team1Text.color = Color.FloralWhite;
 
         Team2Text = Canvas.Add(MatchSystem.instance.Teams[TEAM_TYPE.TEAM_2].Name);
-        Team2Text.position = new Vector2(0.22f, 0.18f);
+        Team2Text.position = new Vector2(0.66f, 0.25f);
+        Team2Text.origin = new Vector2(0.5f);
         Team2Text.color = Color.FloralWhite;
 
         SpectatorText = Canvas.Add(MatchSystem.instance.Teams[TEAM_TYPE.TEAM_SPECTATOR].Name);
-        SpectatorText.position = new Vector2(0.35f, 0.18f);
+        SpectatorText.position = new Vector2(0.85f, 0.15f);
+        SpectatorText.origin = new Vector2(0.5f);
         SpectatorText.color = Color.FloralWhite;
     }
 
