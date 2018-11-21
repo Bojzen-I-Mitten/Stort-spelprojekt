287166a6bbe21aaeba7c309770cefa0132360a2b
Jakob Ahrneteg <jakob.ahrneteg@gmail.com>
2018-11-21 13:58:14 +0100

    Input player name (#153)
    * Input name working, added regex for input text

--- Concussion Ball/Assets/Scripts/Camera/GUI/GUIMainMenu.cs
+++ Concussion Ball/Assets/Scripts/Camera/GUI/GUIMainMenu.cs
@@ -5,29 +5,44 @@ using ThomasEngine;
 
 public class GUIMainMenu : ScriptComponent
 {
+    public Texture2D TextBox { get; set; }
+    public Texture2D TextBoxBG { get; set; }
     public Font TextFont { get; set; }
 
     Camera Camera;
 
     public Canvas Canvas;
 
+    Image TextBoxName;
+    Image TextBoxBGName;
     Text Play;
     Text Options;
     Text Exit;
     Text Credits;
+    Text PlayerName;
+
+    private bool TakeName;
+    public static string PlayerString = "CHAD";
 
     public override void Awake()
     {
+        
     }
 
     public override void Start()
     {
         Camera = gameObject.GetComponent<Camera>();
+        TakeName = false;
         AddImagesAndText();
     }
 
     public override void Update()
     {
+        if(TakeName)
+        {
+            GUIInput.AppendString(ref PlayerString, 8);
+        }
+
         if (!Play.Hovered())
         {
             Play.color = Color.FloralWhite;
@@ -72,21 +87,30 @@ public class GUIMainMenu : ScriptComponent
             //Exit.scale = new Vector2(3.0f);
         }
 
-
         if (Input.GetMouseButtonUp(Input.MouseButtons.LEFT))
         {
+            if (TextBoxName.Clicked())
+            {
+                TakeName = true;
+                TextBoxName.color = Color.Green;
+            }
+
             if (Play.Clicked())
             {
                 CameraMaster.instance.State = CAM_STATE.JOIN_HOST;
             }
         }
+
+        PlayerString = PlayerString.ToUpper();
+        PlayerName.text = PlayerString;
     }
 
     public void AddImagesAndText()
     {
-
         Canvas = Camera.AddCanvas();
 
+        // *** Text ***
+        // Play
         Play = Canvas.Add("Play");
         Play.position = new Vector2(0.425f, 0.11f);
         Play.scale = new Vector2(1.5f);
@@ -95,6 +119,7 @@ public class GUIMainMenu : ScriptComponent
         Play.text = "Play";
         Play.font = TextFont;
 
+        //  Options
         Options = Canvas.Add("Options");
         Options.position = new Vector2(0.425f, 0.21f);
         Options.scale = new Vector2(1.5f);
@@ -103,6 +128,7 @@ public class GUIMainMenu : ScriptComponent
         Options.text = "Options";
         Options.font = TextFont;
 
+        // Credits
         Credits = Canvas.Add("Credits");
         Credits.position = new Vector2(0.425f, 0.31f);
         Credits.scale = new Vector2(1.5f);
@@ -111,6 +137,7 @@ public class GUIMainMenu : ScriptComponent
         Credits.text = "Credits";
         Credits.font = TextFont;
 
+        // Exit
         Exit = Canvas.Add("Exit");
         Exit.position = new Vector2(0.425f, 0.41f);
         Exit.scale = new Vector2(1.5f);
@@ -118,11 +145,48 @@ public class GUIMainMenu : ScriptComponent
         Exit.depth = 0.9f;
         Exit.text = "Exit";
         Exit.font = TextFont;
+
+        // Player name
+        PlayerName = Canvas.Add("PlayerName");
+        PlayerName.position = new Vector2(0.423f, 0.918f);
+        PlayerName.scale = new Vector2(0.9f);
+        PlayerName.interactable = true;
+        PlayerName.depth = 0.8f;
+        PlayerName.text = PlayerString;
+        PlayerName.color = Color.Black;
+        PlayerName.font = TextFont;
+
+        // *** Images ***
+        if (TextBoxBG != null)
+        {
+            TextBoxBGName = Canvas.Add(TextBoxBG);
+            TextBoxBGName.origin = new Vector2(0.5f);
+            TextBoxBGName.position = new Vector2(0.485f, 0.94f);
+            TextBoxBGName.scale = new Vector2(0.65f, 0.65f);
+            TextBoxBGName.depth = 0.9f;
+            TextBoxBGName.color = Color.FloralWhite;
+        }
+
+        if (TextBox != null)
+        {
+            TextBoxName = Canvas.Add(TextBox);
+            TextBoxName.origin = new Vector2(0.5f);
+            TextBoxName.position = new Vector2(0.485f, 0.94f);
+            TextBoxName.scale = new Vector2(0.65f, 0.65f);
+            TextBoxName.interactable = true;
+            TextBoxName.depth = 0.9f;
+            TextBoxName.color = Color.Black;
+        }
     }
 
     public void ClearImagesAndText()
     {
         Canvas.Remove(Play);
+        Canvas.Remove(Options);
+        Canvas.Remove(Credits);
+        Canvas.Remove(Exit);
+        Canvas.Remove(PlayerName);
+        Canvas.Remove(TextBoxBGName);
+        Canvas.Remove(TextBoxName);
     }
-
 }
