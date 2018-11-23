e257bc9150474793ff5b45eae8f0261fbcf5edb9
DESKTOP-KD8IOUS\BTH <oskar.warnhag@gmail.com>
2018-11-23 17:17:13 +0100

    Added basic camera handling for when mouse input is not taken

--- Concussion Ball/Assets/Scripts/Camera/ChadCam.cs
+++ Concussion Ball/Assets/Scripts/Camera/ChadCam.cs
@@ -74,10 +74,22 @@ public class ChadCam : ScriptComponent
                         else
                             ReverseCamera();
                     }
+                    else
+                    {
+                        ResetCamera();
+                        transform.rotation = Chad.transform.rotation;
+                        transform.position = ChadHead + CameraOffset * -transform.forward;
+                    }
                     break;
                 case ChadControls.STATE.THROWING:
                     if (Input.GetMouseMode() == Input.MouseMode.POSITION_RELATIVE)
                         ThrowingCamera();
+                    else
+                    {
+                        ResetCamera();
+                        transform.rotation = Chad.transform.rotation;
+                        transform.position = ChadHead + CameraOffset * -transform.forward;
+                    }
                     break;
                 case ChadControls.STATE.RAGDOLL:
                     {
