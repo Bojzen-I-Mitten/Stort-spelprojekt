28926d435ebc36f9f4889b95fe37ae2d37067986
Albin <albinsahlbom@hotmail.com>
2018-11-26 10:51:11 +0100

    main menu pos fixes

--- Concussion Ball/Assets/Scripts/Camera/GUI/GUIMainMenu.cs
+++ Concussion Ball/Assets/Scripts/Camera/GUI/GUIMainMenu.cs
@@ -79,7 +79,7 @@ public class GUIMainMenu : ScriptComponent
 
         #region  Play
         Play = Canvas.Add("Play");
-        Play.position = new Vector2(0.425f, 0.11f);
+        Play.position = new Vector2(0.1f, 0.11f);
         Play.scale = new Vector2(1.5f);
         Play.interactable = true;
         Play.depth = 0.9f;
@@ -89,7 +89,7 @@ public class GUIMainMenu : ScriptComponent
 
         #region  Options
         Options = Canvas.Add("Options");
-        Options.position = new Vector2(0.425f, 0.21f);
+        Options.position = new Vector2(0.1f, 0.21f);
         Options.scale = new Vector2(1.5f);
         Options.interactable = true;
         Options.depth = 0.9f;
@@ -99,7 +99,7 @@ public class GUIMainMenu : ScriptComponent
 
         #region Credits
         Credits = Canvas.Add("Credits");
-        Credits.position = new Vector2(0.425f, 0.31f);
+        Credits.position = new Vector2(0.1f, 0.31f);
         Credits.scale = new Vector2(1.5f);
         Credits.interactable = true;
         Credits.depth = 0.9f;
@@ -109,7 +109,7 @@ public class GUIMainMenu : ScriptComponent
 
         #region Exit
         Exit = Canvas.Add("Exit");
-        Exit.position = new Vector2(0.425f, 0.41f);
+        Exit.position = new Vector2(0.1f, 0.41f);
         Exit.scale = new Vector2(1.5f);
         Exit.interactable = true;
         Exit.depth = 0.9f;
