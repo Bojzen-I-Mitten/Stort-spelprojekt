384d7f2836dadb93b81f85c6f5ff9ac69457bb91
Vulkan <gustav.ekelund@hotmail.com>
2018-11-28 11:48:51 +0100

    Tweaked shadowbox

--- thomas/ThomasCore/src/thomas/graphics/ChadowMapping.cpp
+++ thomas/ThomasCore/src/thomas/graphics/ChadowMapping.cpp
@@ -38,7 +38,7 @@ namespace thomas
 		ShadowMap::ShadowMap()
 		{
 			m_matrixView = math::Matrix::CreateLookAt(math::Vector3::Up, math::Vector3::Zero, math::Vector3::Up);
-			m_matrixProj = math::Matrix::CreateOrthographicOffCenter(-45, 45, -45, 45, -20, 40);//http://www.opengl-tutorial.org/intermediate-tutorials/tutorial-16-shadow-mapping/
+			m_matrixProj = math::Matrix::CreateOrthographicOffCenter(-45, 45, -45, 45, -45, 45);//http://www.opengl-tutorial.org/intermediate-tutorials/tutorial-16-shadow-mapping/
 			m_matrixVP = m_matrixView * m_matrixProj;
 			_depthStencilView = nullptr;
 		}
