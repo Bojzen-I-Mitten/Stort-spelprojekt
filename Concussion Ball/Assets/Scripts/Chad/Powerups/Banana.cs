197996bc158d7c78e22268fd58812764c8983c7d
Nick Wedzinga <oxay@live.com>
2018-12-03 14:59:27 +0100

    Added Banana Powerup, does nothing right now, doesn't despawn upon collision.

---
+++ Concussion Ball/Assets/Scripts/Chad/Powerups/Banana.cs
@@ -0,0 +1,131 @@
+using ThomasEngine;
+using ThomasEngine.Network;
+using System.Collections;
+using System.Linq;
+
+public class Banana : Powerup
+{
+    ChadControls ObjectOwner = null;
+
+    public float ExplosionRadius { get; set; } = 8.0f;
+    public float ExplosionForce;
+    public float DespawnTime = 60.0f;
+
+    private bool _BananaTriggered = false;
+    private float _BananaTimer = 0.0f;
+
+    public override void OnAwake()
+    {
+        base.OnAwake();
+
+        m_throwable = true; // change depending on power-up
+        MovementSpeedModifier = 1.0f;
+        
+        ExplosionForce = 60.0f;
+        BaseThrowForce = 15.0f;
+        MaxThrowForce = 25.0f;
+        ThrowForce = BaseThrowForce;
+        m_rigidBody.Friction = 100.0f;
+
+        //DespawnTime = 60.0f;
+        //_BananaTriggered = false;
+        //_BananaTimer = 0.0f;
+
+
+        //ExplosionSound = gameObject.AddComponent<SoundComponent>();
+        //ExplosionSound.Type = SoundComponent.SoundType.Effect;
+        //ExplosionSound.Clip = VindalooExplosionSound;
+        //ExplosionSound.Looping = false;
+        //ExplosionSound.Is3D = true;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (_BananaTimer > DespawnTime)
+        {
+            Debug.Log("Banana not triggered for: " + DespawnTime + " seconds.");
+            // Despawn
+        }
+        else if (_BananaTimer > 0)
+            _BananaTimer += Time.DeltaTime;
+    }
+
+    // if this is a throwable power-up this function will be called
+    public override void Throw(Vector3 camPos, Vector3 direction)
+    {
+        _BananaTimer += Time.DeltaTime;
+        base.Throw(camPos, Vector3.Normalize(direction) * ThrowForce);
+    }
+
+    public override void OnCollisionEnter(Collider collider)
+    {
+        //Check if colliding with a player
+        ChadControls otherChad = collider.gameObject.GetComponent<ChadControls>();
+        if (otherChad)
+        {
+            TEAM_TYPE playerTeam = MatchSystem.instance.GetPlayerTeam(ObjectOwner.gameObject);
+            TEAM_TYPE otherPlayerTeam = MatchSystem.instance.GetPlayerTeam(otherChad.gameObject);
+
+            if(playerTeam != otherPlayerTeam)
+                base.OnCollisionEnter(collider);
+        }
+        else
+        {
+            // colliding with static object 
+            Debug.Log("Banana collision with static object: " + collider.gameObject.Name);
+
+            m_rigidBody.Friction = 100.0f;
+            PickupCollider.enabled = true;
+            //Debug.Log("Iskinematic enabled: " + m_rigidBody.IsKinematic);
+            
+        }
+
+    }
+
+    public override void SaveObjectOwner(ChadControls chad)
+    {
+        ObjectOwner = chad;
+    }
+
+    // this function will be called upon powerup use / collision after trown
+    public override void OnActivate()
+    {
+        //Make sure powerups can only be activated once!
+        if (activated)
+            return;
+        activated = true;
+
+        ChadControls otherChad = colliderObject.GetComponent<ChadControls>();
+        if(otherChad)
+        {
+            // rustle his jimmies
+            Ragdoll.ImpactParams param = new Ragdoll.ImpactParams(gameObject.transform.position, Vector3.Zero, 0.0f);
+            param.bodyPartFactor[(int)Ragdoll.BODYPART.SPINE] = 0.88f;
+            otherChad.ActivateRagdoll(4.0f, param);
+        }
+        
+        Explosion();
+    }
+
+    private void Explosion()
+    {
+        // Play the vindaloo explosion sound
+        //ExplosionSound.Play();
+
+        //emitterFire.EmitOneShot(25);
+        //emitterFire2.EmitOneShot(45);
+        //emitterGravel.EmitOneShot(20);
+        //emitterSmoke.EmitOneShot(50);
+
+        StartCoroutine(RemoveNextFrame());
+        //Remove();
+    }
+    private IEnumerator RemoveNextFrame()
+    {
+        yield return null;
+
+        Remove();
+    }
+}
