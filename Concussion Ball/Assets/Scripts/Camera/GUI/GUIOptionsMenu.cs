fab754f6bceed69b183b1558c21dcf7e107dc71c
Unknown <deankulenovic@gmail.com>
2018-12-04 15:00:29 +0100

    Added more options to the Option menu
    Added music,SFX,movement,Aim

--- Concussion Ball/Assets/Scripts/Camera/GUI/GUIOptionsMenu.cs
+++ Concussion Ball/Assets/Scripts/Camera/GUI/GUIOptionsMenu.cs
@@ -6,17 +6,23 @@ public class GUIOptionsMenu : ScriptComponent
 {
     public enum Textstate
     {
-        BACK_TEXTURE,
+        OPTIONS_TEXT,
+        BACK_TEXT,
+        MUSIC_TEXT,
+        SFX_TEXT,
+        MOVEMENTSENSE_TEXT,
+        AIMSENSE_TEXT,
         NUMSTATES
     }
     Camera Camera;
     public Canvas Canvas;
     Text[] Text = new Text[(int)Textstate.NUMSTATES];
-    public Textstate State { set; get; } = Textstate.BACK_TEXTURE;
+    public Textstate State { set; get; } = Textstate.BACK_TEXT;
     public Textstate OldState = Textstate.NUMSTATES;
-    public Font Font { set; get; }
-    public Vector2 SetOrigin{set;get;}
+    public Font Font           { set; get; }
+    public Vector2 SetOrigin   { set; get; }
     public Vector2 SetPosition { set; get; }
+    public Vector2 SetScale { set; get; } = Vector2.One;
     Color Unselected = Color.FloralWhite;
     Color Selected = Color.IndianRed;
     public override void Start()
@@ -27,20 +33,25 @@ public class GUIOptionsMenu : ScriptComponent
     }
     public override void Update()
     {
-
-
         updatePosOrigin();
-        if (Text[(int)Textstate.BACK_TEXTURE].Hovered())
-        {
-            Text[(int)Textstate.BACK_TEXTURE].color = Selected;
-        }
-        else
-            Text[(int)Textstate.BACK_TEXTURE].color = Unselected;
+        ButtonHovered();
+        if (Text[(int)Textstate.BACK_TEXT].Clicked())
+            BackbuttonClicked();
 
-        if (Text[(int)Textstate.BACK_TEXTURE].Clicked())
-            BackbuttonClicked();      
+        if (Input.GetKeyDown(Input.Keys.P))
+            printPosition();
+
     }
-    
+    void ButtonHovered()
+    {
+        for(int i=0;i<(int)Textstate.NUMSTATES;i++)
+        { 
+            if (Text[i].Hovered())
+                Text[i].color = Selected;
+            else
+                Text[i].color = Unselected;
+        }
+    }
     void BackbuttonClicked()
     {
         CameraMaster.instance.State = CAM_STATE.MAIN_MENU;
@@ -48,18 +59,30 @@ public class GUIOptionsMenu : ScriptComponent
     public void AddImagesAndText()
     {
         Canvas = Camera.AddCanvas();
-        Text[(int)Textstate.BACK_TEXTURE] = Canvas.Add("Back");
-        Text[(int)Textstate.BACK_TEXTURE].position = new Vector2(0.1f, 0.2f);
-        foreach (Text Texture in Text)
+        Text[(int)Textstate.BACK_TEXT] = Canvas.Add("Back"); Text[(int)Textstate.BACK_TEXT].position = new Vector2(0.05f, 0.65f); Text[(int)Textstate.BACK_TEXT].interactable = true;
+        Text[(int)Textstate.OPTIONS_TEXT] = Canvas.Add("OPTIONS"); Text[(int)Textstate.OPTIONS_TEXT].scale = new Vector2(1.4f, 1.2f); Text[(int)Textstate.OPTIONS_TEXT].position = new Vector2(0.05f, 0.1f);
+        Text[(int)Textstate.AIMSENSE_TEXT] = Canvas.Add("AIM"); Text[(int)Textstate.AIMSENSE_TEXT].position = new Vector2(0.05f, 0.55f);
+        Text[(int)Textstate.MOVEMENTSENSE_TEXT] = Canvas.Add("Movement"); Text[(int)Textstate.MOVEMENTSENSE_TEXT].position = new Vector2(0.05f, 0.45f);
+        Text[(int)Textstate.MUSIC_TEXT] = Canvas.Add("Music"); Text[(int)Textstate.MUSIC_TEXT].position = new Vector2(0.05f, 0.25f);
+        Text[(int)Textstate.SFX_TEXT] = Canvas.Add("SFX"); Text[(int)Textstate.SFX_TEXT].position = new Vector2(0.05f, 0.35f);
+        for (int i = 0; i < (int)Textstate.NUMSTATES; i++)
         {
-            Texture.interactable = true;
-            Texture.font = Font;
-            Texture.scale = Vector2.One;
+            Text[i].depth = 0.9f;
         }
     }
     public void ClearImagesAndText()
     {
-        Canvas.Remove(Text[(int)Textstate.BACK_TEXTURE]);
+        Canvas.Remove(Text[(int)Textstate.BACK_TEXT]);
+    }
+
+    public void printPosition()
+    {
+        Textstate tempstate = Textstate.OPTIONS_TEXT;
+        foreach (Text Texture in Text)
+        {
+            Debug.Log(tempstate.ToString() + "x = " + Texture.position.x + "   y = " + Texture.position.y);
+            tempstate++;
+        }
     }
     public override void OnDestroy()
     {
@@ -70,11 +93,13 @@ public class GUIOptionsMenu : ScriptComponent
         if(State!= OldState)
         {
            SetPosition = Text[(int)State].position;
+
             OldState = State;
-//          Debug.Log("oldstateUpdated");
+//            Debug.Log(SetPosition.x+" "+SetPosition.y);
         }
         else
-        { 
+        {
+         //   Text[(int)State].scale = SetScale;
             Text[(int)State].origin = SetOrigin;
             Text[(int)State].position = SetPosition;
         }
