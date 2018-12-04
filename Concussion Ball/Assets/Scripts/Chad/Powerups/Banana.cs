da26226ce02d6ddd5a9c1baed65255eba282dd55
Nick Wedzinga <oxay@live.com>
2018-12-04 11:17:31 +0100

    Quick fix

--- Concussion Ball/Assets/Scripts/Chad/Powerups/Banana.cs
+++ Concussion Ball/Assets/Scripts/Chad/Powerups/Banana.cs
@@ -63,7 +63,7 @@ public class Banana : Powerup
             _BananaTimer += Time.DeltaTime;
 
             // If Banana falls on side, reset
-            if (m_rigidBody.LinearVelocity.y < 0.1f && (gameObject.transform.rotation.x > 0.5f || gameObject.transform.rotation.z > 0.5f))
+            if (m_rigidBody.LinearVelocity.y < 0.1f && (gameObject.transform.rotation.x > 0.4f || gameObject.transform.rotation.z > 0.4f))
             {
                 Debug.Log("Banana fell on side");
                 gameObject.transform.rotation = Quaternion.CreateFromYawPitchRoll(0,0,0);
