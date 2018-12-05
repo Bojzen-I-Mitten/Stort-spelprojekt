3f0582efd33d4079fb9d2f23bb136f4359b55c96
DESKTOP-KD8IOUS\BTH <oskar.warnhag@gmail.com>
2018-12-05 09:06:41 +0100

    Removed bad DisconnectAll calls

--- thomas/ThomasNet/NetworkManager.cs
+++ thomas/ThomasNet/NetworkManager.cs
@@ -166,15 +166,12 @@ namespace ThomasEngine.Network
                     Debug.Log("Connection to peer " + peer.EndPoint.ToString() + " timed out");
                     break;
                 case DisconnectReason.ConnectionRejected:
-                    InternalManager.DisconnectAll();
                     Debug.Log("Connection to peer " + peer.EndPoint.ToString() + " rejected");
                     break;
                 case DisconnectReason.ConnectionFailed:
-                    InternalManager.DisconnectAll();
                     Debug.Log("Connection to peer " + peer.EndPoint.ToString() + " failed");
                     break;
                 case DisconnectReason.SocketReceiveError:
-                    InternalManager.DisconnectAll();
                     Debug.Log("Connection to peer " + peer.EndPoint.ToString() + " failed, peer socket closed"); //Could be the other way around
                     break;
                 case DisconnectReason.SocketSendError:
