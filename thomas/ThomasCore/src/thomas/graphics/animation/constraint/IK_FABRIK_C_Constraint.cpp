f6c30f633e4d05eae69073c0bf51b85c2e5ebca6
Mattias <mattias-fredriksson91@hotmail.se>
2018-12-10 08:49:36 +0100

    Them spacings

--- thomas/ThomasCore/src/thomas/graphics/animation/constraint/IK_FABRIK_C_Constraint.cpp
+++ thomas/ThomasCore/src/thomas/graphics/animation/constraint/IK_FABRIK_C_Constraint.cpp
@@ -215,7 +215,7 @@ namespace thomas {
 					trans = pose.Translation();
 					pose.Translation(math::Vector3::Zero);										// Remove translation
 					pose = pose * math::getMatrixRotationTo(pose.Up(), p[i + 1] - p[i]);		// Rotate bone Y toward child's point
-					//pose = pose * skel.getBone(chain[i + 1])._invParentOrient;					// Apply orientation offset in relation to child (Y axis of the bone may not face child)
+					//pose = pose * skel.getBone(chain[i + 1])._invParentOrient;				// Apply orientation offset in relation to child (Y axis of the bone may not face child)
 					pose.Translation(p[i]);														// Apply new translation
 					objectPose[chain[i]] = pose;												// Set
 				}
